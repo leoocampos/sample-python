d299a64993e095cefe747a39031a9f9d6a4716ca
leoocampos <leofernandescampos@gmail.com>
2026-02-09 22:03:39 -0300

    Add comment

--- main.py
+++ main.py
@@ -9,5 +9,5 @@ def index():
 
 
 if __name__ == "__main__":
-    # Dev only: run "python main.py" and open http://localhost:8080
+    # mensagem pro Dev: run "python main.py" and open http://localhost:8080
     app.run(host="localhost", port=8080, debug=True)
